324c1e78d52e568dd99b83df259c4512c87d3e97
James Wilcox <jrw12@cs.washington.edu>
2018-05-02 12:13:32 -0700

    fixes to spec_properties.v

--- spec_properties.v
+++ spec_properties.v
@@ -16,21 +16,23 @@ Definition ser_deser_spec A
 
 (*
 
-property 1: deserialize(serialize(a)) = Some(a, []);
+property 1: forall a, deserialize(serialize(a)) = Some(a, []);
 
-property 2: deserialize(bools) = Some(a, bools') ->
+property 2: forall bools...,
+            deserialize(bools) = Some(a, bools') ->
             deserialize(bools ++ bools2) = Some(a, bools' ++ bools2)
 
 *)
 
-Theorem deser_spec_decompose: forall (A : Type) (ser : serializer A) (deser : deserializer A)
-    (a: A) (bools leftover other: list bool),
-   ((deser (ser a)) = Some(a, [])
-/\ ((deser bools) = Some(a, leftover) -> (deser (bools ++ other)) = Some (a, leftover ++ other))) ->
-  ser_deser_spec A ser deser.
+Theorem deser_spec_decompose :
+  forall (A : Type) (ser : serializer A) (deser : deserializer A),
+    (forall a, deser (ser a) = Some (a, [])) ->
+    (forall a (bools leftover other: list bool),
+        deser bools = Some (a, leftover) ->
+        deser (bools ++ other) = Some (a, leftover ++ other)) ->
+    ser_deser_spec A ser deser.
 Proof.
   unfold ser_deser_spec.
-  intros A ser deser a bools leftover other props.
-  inversion props.
-  intros.
-  rewrite H0.
+  intros A ser deser P1 P2 a bools.
+  now erewrite P2 by auto.
+Qed.
